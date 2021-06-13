6f4e2e77b05a90b97bb16895241a83655acdcec7
Tyler Lai <lai.t.tyler@gmail.com>
2021-06-13 16:52:33 -0500

    feat: Add react-hook-form

--- components/shared/Button/index.js
+++ components/shared/Button/index.js
@@ -1,4 +1,4 @@
-const Button = ({ children, onClick, icon, primary }) => {
+const Button = ({ children, icon, onClick, primary, type }) => {
   const btnColor = primary ? "bg-gray-600" : "bg-white";
   const btnTextColor = primary ? "text-white" : "text-gray-500";
 
@@ -6,6 +6,7 @@ const Button = ({ children, onClick, icon, primary }) => {
     <button
       className={`flex items-center justify-start ${btnTextColor} text-sm ${btnColor} px-5 py-2 mt-2 font-medium transition duration-300 ease-in-out transform  rounded-md focus:shadow-outline hover:bg-gray-800 hover:text-white hover:border-gray-800 focus:outline-none`}
       onClick={onClick}
+      type={type}
     >
       {icon && <span className="mr-2">{icon}</span>}
       {children}
--- components/shared/Modal/index.js
+++ components/shared/Modal/index.js
@@ -1,11 +1,19 @@
+import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
-import { openModal, closeModal } from "./modalSlice";
+import { closeModal } from "./modalSlice";
 import { Dialog, Transition } from "@headlessui/react";
 import Button from "../Button";
 
 const Modal = () => {
   const isOpen = useSelector((state) => state.modal.isOpen);
   const dispatch = useDispatch();
+
+  const { register, handleSubmit, reset } = useForm();
+
+  const onSubmit = (data, e) => {
+    console.log(data);
+    reset();
+  };
 
   return (
     <Transition show={isOpen}>
@@ -17,42 +25,53 @@ const Modal = () => {
         <div className="flex items-center justify-center min-h-screen">
           <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
 
-          <div className="bg-white rounded max-w-sm mx-auto z-20 p-6">
+          <div className="bg-white rounded w-3/4 sm:w-1/3 mx-auto z-20 p-6">
             <Dialog.Title>
-              <p className="text-2xl bold text-gray-800">Add new person</p>
+              <p className="text-2xl bold text-gray-800">Add Person Form</p>
             </Dialog.Title>
-            <Dialog.Description>Add a new person to your People Pod</Dialog.Description>
-            <div className="flex flex-col space-y-3 my-5">
+            <Dialog.Description className="text-sm text-gray-500 mb-5">
+              Add someone new to your People Pod.
+            </Dialog.Description>
+            <div>
               <p className="text-xl font-bold text-gray-800">Full name</p>
-              <label htmlFor="firstName">First Name</label>
-              <input
-                className="border border-gray-200 rounded pl-4 py-1"
-                id="firstName"
-                name="firstName"
-                type="text"
-                placeholder="First name..."
-              />
-              <label htmlFor="lastName">Last Name</label>
-              <input
-                className="border border-gray-200 rounded pl-4 py-1"
-                id="lastName"
-                name="lastName"
-                type="text"
-                placeholder="Last name..."
-              />
-              <label htmlFor="quickNote">Quick Note</label>
-              <textarea
-                className="border border-gray-200 rounded pl-4 py-1"
-                id="quickNote"
-                name="quickNote"
-                type="text"
-                placeholder="Quick note..."
-              />
-            </div>
-
-            <div className="flex justify-between">
-              <Button primary onClick={() => dispatch(closeModal())}>Add Person</Button>
-              <Button onClick={() => dispatch(closeModal())}>Cancel</Button>
+              <form
+                className="flex flex-col space-y-3 mb-5"
+                onSubmit={handleSubmit(onSubmit)}
+              >
+                <label htmlFor="firstName">First Name</label>
+                <input
+                  className="border border-gray-200 rounded pl-4 py-1"
+                  id="firstName"
+                  name="firstName"
+                  type="text"
+                  placeholder="First name..."
+                  {...register("firstName")}
+                />
+                <label htmlFor="lastName">Last Name</label>
+                <input
+                  className="border border-gray-200 rounded pl-4 py-1"
+                  id="lastName"
+                  name="lastName"
+                  type="text"
+                  placeholder="Last name..."
+                  {...register("lastName")}
+                />
+                <label htmlFor="quickNote">Quick Note</label>
+                <textarea
+                  className="border border-gray-200 rounded pl-4 py-1"
+                  id="quickNote"
+                  name="quickNote"
+                  type="text"
+                  placeholder="Quick note..."
+                  {...register("quickNote")}
+                />
+                <div className="flex justify-between">
+                  <Button onClick={onSubmit} type="submit">
+                    Add Person
+                  </Button>
+                  <Button onClick={() => dispatch(closeModal())}>Cancel</Button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
