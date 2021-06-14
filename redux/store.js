80f129176ebb58b7d3438834262ce405dd624fc8
Tyler Lai <lai.t.tyler@gmail.com>
2021-06-14 02:29:34 -0500

    refactor: Update folder structure for redux and slices

--- redux/store.js
+++ redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import modalReducer from "../components/shared/Modal/modalSlice";
+import modalReducer from "./slices/modalSlice";
 
 export const store = configureStore({
   reducer: {
