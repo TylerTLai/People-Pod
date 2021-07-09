6e96a42278bb99b52a2edbcc7404a57c9ead8e97
Tyler Lai <lai.t.tyler@gmail.com>
2021-07-09 03:04:44 -0500

    fix: Fix grid view picture size

--- components/PeopleList/PersonCard/PersonPic/index.js
+++ components/PeopleList/PersonCard/PersonPic/index.js
@@ -3,7 +3,7 @@ const PersonPic = ({ listView }) => {
     <img
       src="https://dummyimage.com/200x200/F3F4F7/8693ac"
       alt="placeholder"
-      className={`rounded-md h-full w-${listView ? "32" : "full"} pt-2`}
+      className={`rounded-md h-full w-full ${listView && "w-32"} pt-2`}
     />
   );
 };
