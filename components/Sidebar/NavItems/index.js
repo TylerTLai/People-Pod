1d60d0c2bf4c974836494ff64eacbed75cbb0ab5
Tyler Lai <lai.t.tyler@gmail.com>
2021-07-08 23:49:46 -0500

    refactor: Update sidebar design

---
+++ components/Sidebar/NavItems/index.js
@@ -0,0 +1,30 @@
+import { FiUsers } from "react-icons/fi";
+import { AiOutlineHeart } from "react-icons/Ai";
+const NavItems = ({ showSidebar }) => {
+  return (
+    <nav className={`${!showSidebar && "hidden"} min-h-screen text-gray-300`}>
+      <ul>
+        <li>
+          <a
+            className="flex items-center px-4 py-2 mt-2 transition duration-300 ease-in-out border-l-4 border-transparent hover:border-white focus:outline-none hover:text-white"
+            href="#"
+          >
+            <FiUsers size={20} />
+            <p className="ml-4">Everyone</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center px-4 py-2 mt-2 transition duration-300 ease-in-out border-l-4 border-transparent hover:border-white focus:outline-none hover:text-white"
+            href="#"
+          >
+            <AiOutlineHeart size="18" />
+            <p className="ml-4">Favorites</p>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavItems;
--- components/Sidebar/index.js
+++ components/Sidebar/index.js
@@ -1,58 +1,41 @@
-import { FiPlusCircle, FiUsers } from "react-icons/fi";
+import { FiPlusCircle, FiUsers, FiSidebar } from "react-icons/fi";
 import { AiOutlineHeart } from "react-icons/Ai";
+import { useState } from "react";
 import Link from "next/link";
+import NavItems from "./NavItems";
+import UserProfile from "./UserProfile";
 
 const Sidebar = () => {
+  const [showSidebar, setShowSidebar] = useState(true);
+
+  const handleSidebarToggle = () => {
+    setShowSidebar((prev) => !prev);
+  };
+
   return (
-    <div className="flex-col w-full md:flex md:flex-row md:min-h-screen pt-6 border-r border-gray-200">
-      <div className="flex flex-col flex-shrink-0 w-full md:w-64">
-        <div className="flex items-center">
-          <p className="font-bold uppercase tracking-wide ml-5">Groups</p>
-          <button
-            className="p-1 m-2 ml-auto focus:outline-none"
-            type="button"
-            aria-label="new group"
-            aria-hidden="true"
-          >
-            <FiPlusCircle size={24} color="#1F2937" />
-          </button>
-        </div>
-        <nav className="flex-grow pb-4 pr-4 md:block md:pb-0 md:overflow-y-auto">
-          <ul className="text-gray-500">
-            <li>
-              <a
-                className="inline-flex items-center px-4 py-2 mt-2 transition duration-300 ease-in-out border-l-4 hover:border-gray-800 focus:outline-none hover:text-gray-800"
-                href="#"
-              >
-                <FiUsers size={20} />
-                <span className="ml-4">Everyone</span>
-              </a>
-            </li>
-            <li>
-              <a
-                className="inline-flex items-center px-4 py-2 mt-2 transition duration-300 ease-in-out border-l-4 hover:border-gray-800 focus:outline-none hover:text-gray-800"
-                href="#"
-              >
-                <AiOutlineHeart size="18" />
-                <span className="ml-4">Favorites</span>
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <div className="flex items-center w-full p-4 rounded-lg ">
-          <img
-            src="https://dummyimage.com/302x302/F3F4F7/8693ac"
-            alt="user name"
-            className="object-cover w-10 h-10 mr-4 rounded-full"
-          />
-          <div>
-            <h2 className="text-sm font-medium tracking-widest text-gray-800 uppercase title-font">
-              John Doe
-            </h2>
-            <p className="text-blueGray-500">Admin</p>
-          </div>
-        </div>
+    <div
+      className={`flex flex-col pt-6 bg-gray-800 text-white w-${
+        showSidebar ? "64" : "0.5"
+      }`}
+    >
+      <FiSidebar
+        className={`ml-5 ${!showSidebar && "mr-5"} mb-9 mt-3 hover:cursor-pointer`}
+        size={24}
+        onClick={handleSidebarToggle}
+      />
+      <div className={`flex items-center ${!showSidebar && "hidden"}`}>
+        <p className="font-bold uppercase tracking-wide ml-5">Groups</p>
+        <button
+          className="p-1 m-2 ml-auto focus:outline-none"
+          type="button"
+          aria-label="new group"
+          aria-hidden="true"
+        >
+          <FiPlusCircle size={24} color="#FFFFFF" />
+        </button>
       </div>
+      <NavItems showSidebar={showSidebar} />
+      <UserProfile showSidebar={showSidebar} />
     </div>
   );
 };
