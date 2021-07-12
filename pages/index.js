08e076e73f989a16d251ae013d02697c94473786
Tyler Lai <lai.t.tyler@gmail.com>
2021-07-11 22:48:19 -0500

    refactor: Create dashboard route and update Home route

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,16 @@
-import { useEffect } from "react";
-import Head from "next/head";
-import Layout from "../components/Layout";
-import Navbar from "../components/Navbar";
-import Sidebar from "../components/Sidebar";
-import PeopleList from "../components/PeopleList";
-import PersonDetails from "../components/PersonDetails";
-import Modal from "../components/shared/Modal";
-import axiosInstance from "../config/axios";
-import { useDispatch } from "react-redux";
-import { setAllPeople } from "../redux/slices/peopleSlice";
+import Link from "next/link";
+import Button from "../components/shared/Button";
 
-const Home = ({ data }) => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(setAllPeople(data));
-  }, []);
-
+const Home = () => {
   return (
     <>
-      <Modal />
-      <Navbar />
-      <Layout>
-        <Sidebar />
-        <PeopleList />
-        <PersonDetails />
-      </Layout>
+      <div>
+        <Link href="/dashboard">
+          <Button>Dashboard</Button>
+        </Link>
+      </div>
     </>
   );
 };
-
-export async function getServerSideProps() {
-  const response = await axiosInstance.get("people");
-  let data = response.data;
-  return {
-    props: {
-      data,
-    },
-  };
-}
 
 export default Home;
