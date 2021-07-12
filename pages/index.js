4f5a882f7f3defde18134c530813cb759b6cdf9b
Tyler Lai <lai.t.tyler@gmail.com>
2021-07-12 02:04:15 -0500

    fix: Fix class name

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ const Home = () => {
         </p>
 
         <h1
-          class="text-4xl lg:text-5xl font-bold
+          className="text-4xl lg:text-5xl font-bold
         text-gray-900 mb-10 text-center"
         >
           Remember the things that matter
