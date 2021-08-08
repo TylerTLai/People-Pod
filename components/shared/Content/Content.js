fbc9515008c5bb5bf4eb7c09a71a2fd1cab6cdd6
Tyler Lai <lai.t.tyler@gmail.com>
2021-08-08 16:49:33 -0500

    feat: Update landing page design

---
+++ components/shared/Content/Content.js
@@ -0,0 +1,178 @@
+const Content = () => {
+  return (
+    <div>
+      <section className="text-gray-700 ">
+        <div className="container flex flex-col items-center px-5 py-8 mx-auto">
+          <div className="flex flex-col w-full mb-12 text-left "></div>
+          <div className="flex flex-col items-center pb-10 mx-auto mb-10 border-b border-gray-200 sm:flex-row lg:w-1/2">
+            <div className="inline-flex items-center justify-center flex-shrink-0 w-20 h-20 text-black rounded-full bg-gray-50 sm:mr-10">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="1.5"
+                className="w-10 h-10"
+                viewBox="0 0 24 24"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <circle cx="12" cy="12" r="9"></circle>
+                <line x1="3.6" y1="15" x2="14.15" y2="15"></line>
+                <line
+                  x1="3.6"
+                  y1="15"
+                  x2="14.15"
+                  y2="15"
+                  transform="rotate(72 12 12)"
+                ></line>
+                <line
+                  x1="3.6"
+                  y1="15"
+                  x2="14.15"
+                  y2="15"
+                  transform="rotate(144 12 12)"
+                ></line>
+                <line
+                  x1="3.6"
+                  y1="15"
+                  x2="14.15"
+                  y2="15"
+                  transform="rotate(216 12 12)"
+                ></line>
+                <line
+                  x1="3.6"
+                  y1="15"
+                  x2="14.15"
+                  y2="15"
+                  transform="rotate(288 12 12)"
+                ></line>
+              </svg>
+            </div>
+            <div className="flex-grow mt-6 text-center sm:text-left sm:mt-0">
+              <h2 className="mb-8 text-2xl font-semibold leading-none tracking-tighter text-black lg:text-3xl title-font">
+                {" "}
+                Add People.{" "}
+              </h2>
+              <p className="text-base font-medium leading-relaxed text-gray-700">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam odit
+                rem tempora accusamus deserunt debitis magnam placeat ut amet dolorem.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center pb-10 mx-auto mb-10 border-b border-gray-200 sm:flex-row lg:w-1/2">
+            <div className="inline-flex items-center justify-center flex-shrink-0 w-20 h-20 text-black rounded-full bg-gray-50 sm:mr-10">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="1.5"
+                className="w-10 h-10"
+                viewBox="0 0 24 24"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <circle cx="12" cy="12" r="9"></circle>
+                <line x1="3.6" y1="15" x2="14.15" y2="15"></line>
+                <line
+                  x1="3.6"
+                  y1="15"
+                  x2="14.15"
+                  y2="15"
+                  transform="rotate(72 12 12)"
+                ></line>
+                <line
+                  x1="3.6"
+                  y1="15"
+                  x2="14.15"
+                  y2="15"
+                  transform="rotate(144 12 12)"
+                ></line>
+                <line
+                  x1="3.6"
+                  y1="15"
+                  x2="14.15"
+                  y2="15"
+                  transform="rotate(216 12 12)"
+                ></line>
+                <line
+                  x1="3.6"
+                  y1="15"
+                  x2="14.15"
+                  y2="15"
+                  transform="rotate(288 12 12)"
+                ></line>
+              </svg>
+            </div>
+            <div className="flex-grow mt-6 text-center sm:text-left sm:mt-0">
+              <h2 className="mb-8 text-2xl font-semibold leading-none tracking-tighter text-black lg:text-3xl title-font">
+                {" "}
+                Add Notes.{" "}
+              </h2>
+              <p className="text-base font-medium leading-relaxed text-gray-700">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam odit
+                rem tempora accusamus deserunt debitis magnam placeat ut amet dolorem.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center pb-10 mx-auto mb-10 border-b border-gray-200 sm:flex-row lg:w-1/2">
+            <div className="inline-flex items-center justify-center flex-shrink-0 w-20 h-20 text-black rounded-full bg-gray-50 sm:mr-10">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="1.5"
+                className="w-10 h-10"
+                viewBox="0 0 24 24"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <circle cx="12" cy="12" r="9"></circle>
+                <line x1="3.6" y1="15" x2="14.15" y2="15"></line>
+                <line
+                  x1="3.6"
+                  y1="15"
+                  x2="14.15"
+                  y2="15"
+                  transform="rotate(72 12 12)"
+                ></line>
+                <line
+                  x1="3.6"
+                  y1="15"
+                  x2="14.15"
+                  y2="15"
+                  transform="rotate(144 12 12)"
+                ></line>
+                <line
+                  x1="3.6"
+                  y1="15"
+                  x2="14.15"
+                  y2="15"
+                  transform="rotate(216 12 12)"
+                ></line>
+                <line
+                  x1="3.6"
+                  y1="15"
+                  x2="14.15"
+                  y2="15"
+                  transform="rotate(288 12 12)"
+                ></line>
+              </svg>
+            </div>
+            <div className="flex-grow mt-6 text-center sm:text-left sm:mt-0">
+              <h2 className="mb-8 text-2xl font-semibold leading-none tracking-tighter text-black lg:text-3xl title-font">
+                {" "}
+                Add Groups.{" "}
+              </h2>
+              <p className="text-base font-medium leading-relaxed text-gray-700">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam odit
+                rem tempora accusamus deserunt debitis magnam placeat ut amet dolorem.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Content;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import SiteNavbar from "../components/Navbars/SiteNavbar";
 import { useSession } from "next-auth/client";
+import Content from "../components/shared/Content/Content";
+import Footer from "../components/shared/Footer/Footer";
 
 const Home = () => {
   const [session, loading] = useSession();
@@ -16,7 +18,7 @@ const Home = () => {
         <>
           <SiteNavbar />
           <section className="text-center text-2xl text-gray-500 leading-normal py-24">
-            <p className="text-center font-bold uppercase text-xl mb-4 text-green-300 tracking-widest">
+            <p className="text-center font-bold uppercase text-xl mb-4 text-blue-500 tracking-widest">
               A modern personal CRM
             </p>
 
@@ -36,101 +38,8 @@ const Home = () => {
             </p>
           </section>
 
-          <div className="px-24">
-            <section className="py-16 flex justify-between items-center">
-              <div>
-                <h1 className="text-left font-bold text-4xl text-red-600 uppercase tracking-wide mb-2">
-                  Manage
-                </h1>
-                <p className="text-left text-gray-500">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  <br /> Sunt mollitia, alias, veniam consequatur repellendus obcaecati
-                  <br /> vel quidem aspernatur ipsum quaerat officia perferendis
-                  <br /> voluptates a enim explicabo sed et asperiores molestias?
-                </p>
-              </div>
-              <div>
-                <img
-                  src="https://dummyimage.com/200x200/F3F4F7/8693ac"
-                  alt="placeholder"
-                  className={`rounded-md h-full w-full`}
-                />
-              </div>
-            </section>
-
-            <section className=" py-16 flex justify-between items-center">
-              <div>
-                <img
-                  src="https://dummyimage.com/200x200/F3F4F7/8693ac"
-                  alt="placeholder"
-                  className={`rounded-md h-full w-full`}
-                />
-              </div>
-              <div>
-                <h1 className="text-right font-bold text-4xl text-blue-500 uppercase tracking-wide mb-2">
-                  Organize
-                </h1>
-                <p className="text-right text-gray-500">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  <br /> Sunt mollitia, alias, veniam consequatur repellendus obcaecati
-                  <br /> vel quidem aspernatur ipsum quaerat officia perferendis
-                  <br /> voluptates a enim explicabo sed et asperiores molestias? lorem
-                </p>
-              </div>
-            </section>
-
-            <section className="py-16 flex justify-between items-center">
-              <div>
-                <h1 className="text-left font-bold text-4xl text-yellow-500 uppercase tracking-wide mb-2">
-                  Track
-                </h1>
-                <p className="text-left text-gray-500">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  <br /> Sunt mollitia, alias, veniam consequatur repellendus obcaecati
-                  <br /> vel quidem aspernatur ipsum quaerat officia perferendis
-                  <br /> voluptates a enim explicabo sed et asperiores molestias?
-                </p>
-              </div>
-              <div>
-                <img
-                  src="https://dummyimage.com/200x200/F3F4F7/8693ac"
-                  alt="placeholder"
-                  className={`rounded-md h-full w-full`}
-                />
-              </div>
-            </section>
-          </div>
-
-          <div className="px-24">
-            <section className="py-16 flex justify-between items-center">
-              <div className="grid grid-cols-3 gap-y-40 gap-x-10">
-                <div className="text-white bg-gray-900 rounded-md py-10 px-10 text-center">
-                  <h2 className="font-bold text-xl uppercase mb-2">Add people</h2>
-                  <p className="text-gray-300">
-                    Debitis velit iure non, excepturi quo maxime quos perspiciatis hic
-                    harum cum, reiciendis deserunt error esse ipsa.
-                  </p>
-                </div>
-                <div className="text-white bg-gray-900 rounded-md py-10 px-10 text-center">
-                  <h2 className="font-bold text-xl uppercase mb-2">Add notes</h2>
-                  <p className="text-gray-300">
-                    Debitis velit iure non, excepturi quo maxime quos perspiciatis hic
-                    harum cum, reiciendis deserunt error esse ipsa.
-                  </p>
-                </div>
-                <div className="text-white bg-gray-900 rounded-md py-10 px-10 text-center">
-                  <h2 className="font-bold text-xl uppercase mb-2">Create groups</h2>
-                  <p className="text-gray-300">
-                    Debitis velit iure non, excepturi quo maxime quos perspiciatis hic
-                    harum cum, reiciendis deserunt error esse ipsa.
-                  </p>
-                </div>
-              </div>
-            </section>
-          </div>
-          <footer className="pt-44 pb-10 text-center text-sm text-gray-500 tracking-wide">
-            Designed & built by Tyler Lai
-          </footer>
+          <Content />
+          <Footer />
         </>
       )}
     </div>
