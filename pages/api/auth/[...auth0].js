7ad6f3153584c0d5c1a9d80cfbacf85c7bb6fe3d
Tyler Lai <lai.t.tyler@gmail.com>
2021-08-15 01:47:21 -0500

    refactor: Update auth to use nextjs-auth

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,3 +1,23 @@
-import { handleAuth } from "@auth0/nextjs-auth0";
+import { handleAuth, handleCallback } from "@auth0/nextjs-auth0";
+import { createUser } from "../../../utils/db";
 
-export default handleAuth();
+const afterCallback = async (req, res, session, state) => {
+
+  try {
+    await createUser(session.user.email);
+  } catch (err) {
+    console.log(err);
+  }
+
+  return session;
+};
+
+export default handleAuth({
+  async callback(req, res) {
+    try {
+      await handleCallback(req, res, { afterCallback });
+    } catch (error) {
+      res.status(error.status || 500).end(error.message);
+    }
+  },
+});
