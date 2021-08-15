408ba81558b8923891c46eb8974584c9b728f5c4
Tyler Lai <lai.t.tyler@gmail.com>
2021-08-15 01:50:07 -0500

    refactor: Update dashboard and navbar auth

--- components/Navbars/DashboardNavbar/index.js
+++ components/Navbars/DashboardNavbar/index.js
@@ -1,11 +1,14 @@
 import Link from "next/link";
-import SvgSearch from "../../shared/Icons/Search";
 import { useDispatch } from "react-redux";
+import { useUser } from "@auth0/nextjs-auth0";
+
 import { filterPeople } from "../../../redux/slices/peopleSlice";
+import SvgSearch from "../../shared/Icons/Search";
 import UserProfile from "../../Sidebar/UserProfile";
 
 const DashboardNavbar = () => {
   const dispatch = useDispatch();
+  const { user, error, isLoading } = useUser();
 
   const handleInputChange = (e) => {
     const searchTerm = e.target.value.toLowerCase();
