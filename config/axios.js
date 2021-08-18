3f69b00b35451356b5e27e6f077e86ecaf48d9bd
Tyler Lai <lai.t.tyler@gmail.com>
2021-08-17 23:38:24 -0500

    refactor: Update axios instance baseURL

--- config/axios.js
+++ config/axios.js
@@ -1,7 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: `${
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/api/"
+      : "https://people-pod.vercel.app/api"
+  }`,
 });
 
 export default axiosInstance;
