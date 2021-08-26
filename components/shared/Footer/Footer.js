5ad17aa20621c1f8daa95d28aff3796fa017599e
Tyler Lai <lai.t.tyler@gmail.com>
2021-08-25 19:53:18 -0500

    refactor: Clean up Footer class

--- components/shared/Footer/Footer.js
+++ components/shared/Footer/Footer.js
@@ -1,104 +1,84 @@
 const Footer = () => {
   return (
-    <div>
-      <div className="container items-center">
-        <footer className="text-gray-700 transition duration-500 ease-in-out transform bg-white rounded-lg ">
-          <div className="flex flex-col flex-wrap justify-center p-5 md:flex-row">
-            <nav className="flex flex-wrap items-center justify-center w-full mx-auto mb-6 text-base nprd">
-              <a
-                href="#"
-                className="px-4 py-1 mr-1 text-base text-gray-500 transition duration-500 ease-in-out transform rounded-md focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 hover:text-black "
-              >
-                Pricing
-              </a>
-              <a
-                href="#"
-                className="px-4 py-1 mr-1 text-base text-gray-500 transition duration-500 ease-in-out transform rounded-md focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 hover:text-black "
-              >
-                Contact
-              </a>
-              <a
-                href="#"
-                className="px-4 py-1 mr-1 text-base text-gray-500 transition duration-500 ease-in-out transform rounded-md focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 hover:text-black "
-              >
-                Services
-              </a>
-              <a
-                href="#"
-                className="px-4 py-1 mr-1 text-base text-gray-500 transition duration-500 ease-in-out transform rounded-md focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 hover:text-black "
-              >
-                Now
-              </a>
-            </nav>
-            <span className="inline-flex justify-center w-full mx-auto mt-2 mr-2 sm:ml-auto sm:mt-0">
-              <a className="text-gray-700 hover:text-black">
-                <svg
-                  fill="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                </svg>
-              </a>
-              <a className="ml-3 text-gray-700 hover:text-black">
-                <svg
-                  fill="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                </svg>
-              </a>
-              <a className="ml-3 text-gray-700 hover:text-black">
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                  <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                </svg>
-              </a>
-              <a className="ml-3 text-gray-700 hover:text-black">
-                <svg
-                  fill="currentColor"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="0"
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke="none"
-                    d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
-                  ></path>
-                  <circle cx="4" cy="4" r="2" stroke="none"></circle>
-                </svg>
-              </a>
-            </span>
-          </div>
-          <div className="w-full px-8 mt-4">
-            <div className="container inline-flex flex-col flex-wrap items-center px-5 py-6 mx-auto sm:flex-row">
-              <p className="mx-auto text-sm text-center text-black sm:text-left ">
-                {" "}
-                © 2021{" "}
-              </p>
-            </div>
-          </div>
-        </footer>
+    <footer className="text-gray-400 py-10">
+      <div>
+        <nav className="flex justify-center text-base space-x-8 text-gray-600 py-2">
+          <a href="#" className="hover:text-gray-900 transition duration-200 ease-in-out">
+            Pricing
+          </a>
+          <a href="#" className="hover:text-gray-900 transition duration-200 ease-in-out">
+            Contact
+          </a>
+          <a href="#" className="hover:text-gray-900 transition duration-200 ease-in-out">
+            Services
+          </a>
+          <a href="#" className="hover:text-gray-900 transition duration-200 ease-in-out">
+            Now
+          </a>
+        </nav>
+        <span className="flex justify-center space-x-8 py-2">
+          <a className="text-gray-400 hover:text-black transition duration-200 ease-in-out hover:cursor-pointer">
+            <svg
+              fill="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-5 h-5"
+              viewBox="0 0 24 24"
+            >
+              <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+            </svg>
+          </a>
+          <a className="ml-3 text-gray-400 hover:text-black transition duration-200 ease-in-out hover:cursor-pointer">
+            <svg
+              fill="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-5 h-5"
+              viewBox="0 0 24 24"
+            >
+              <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+            </svg>
+          </a>
+          <a className="ml-3 text-gray-400 hover:text-black transition duration-200 ease-in-out hover:cursor-pointer">
+            <svg
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-5 h-5"
+              viewBox="0 0 24 24"
+            >
+              <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+              <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+            </svg>
+          </a>
+          <a className="ml-3 text-gray-400 hover:text-black transition duration-200 ease-in-out hover:cursor-pointer">
+            <svg
+              fill="currentColor"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="0"
+              className="w-5 h-5"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="none"
+                d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+              ></path>
+              <circle cx="4" cy="4" r="2" stroke="none"></circle>
+            </svg>
+          </a>
+        </span>
       </div>
-    </div>
+
+      <div className="flex justify-center pt-5">
+        <p className="text-sm text-gray-400 ">© 2021</p>
+      </div>
+    </footer>
   );
 };
 
