6b4d31bc129633b370e749b9be5cba8b474f4f4e
Tyler Lai <lai.t.tyler@gmail.com>
2021-10-11 21:32:04 -0500

    refactor: Add Socail data to person

--- components/PersonDetails/helper.js
+++ components/PersonDetails/helper.js
@@ -1,25 +1,30 @@
 import { v4 as uuidv4 } from "uuid";
 import SvgCake from "../shared/Icons/Cake";
 import SvgClipboard from "../shared/Icons/Clipboard";
+import SvgFacebook from "../shared/Icons/Facebook";
+import SvgWebsite from "../shared/Icons/Globe";
 import SvgHome from "../shared/Icons/Home";
+import SvgInstagram from "../shared/Icons/Instagram";
+import SvgLinkedin from "../shared/Icons/Linkedin";
 import SvgMail from "../shared/Icons/Mail";
 import SvgMapPin from "../shared/Icons/MapPin";
 import SvgSmartphone from "../shared/Icons/Smartphone";
+import SvgTwitter from "../shared/Icons/Twitter";
 
 export const createSectionData = (person) => {
   const sectionData = [];
 
   sectionData.push({
-    title: "About",
-    details: [
+    aboutTitle: "About",
+    aboutDetails: [
       {
         id: uuidv4(),
-        value: person.quickNote ? person.quickNote : "No notes provided.",
+        value: person.quickNote ? person.quickNote : "No notes provided",
         icon: SvgClipboard,
       },
       {
         id: uuidv4(),
-        value: person.birthday ? person.birthday : "No birthday provided.",
+        value: person.birthday ? person.birthday : "No birthday provided",
         icon: SvgCake,
       },
       {
@@ -43,34 +48,35 @@ export const createSectionData = (person) => {
         icon: SvgMail,
       },
     ],
+    socialTitle: "Social",
+    socialDetails: [
+      {
+        id: uuidv4(),
+        value: person.facebookId ? person.facebookId : "No Facebook provided",
+        icon: SvgFacebook,
+      },
+      {
+        id: uuidv4(),
+        value: person.twitterId ? person.twitterId : "No Twitter provided",
+        icon: SvgTwitter,
+      },
+      {
+        id: uuidv4(),
+        value: person.instagramId ? person.instagramId : "No Instagram provided",
+        icon: SvgInstagram,
+      },
+      {
+        id: uuidv4(),
+        value: person.linkedinId ? person.linkedinId : "No LinkedIn provided",
+        icon: SvgLinkedin,
+      },
+      {
+        id: uuidv4(),
+        value: person.website ? person.website : "No website provided",
+        icon: SvgWebsite,
+      },
+    ],
   });
-
-  // personData currently:
-
-  // id: 93
-  // personId: "e27c036b-0583-4363-808d-1fee07cea594"
-  // firstName: "test"
-  // lastName: "123"
-  // favorite: false
-  // quickNote: "abc"
-  // userId: "ckrdw9x400008w2vk5j47mlqm"
-  // groups: []
-
-  // Need to change personData to something like this:
-
-  // id: 93
-  // personId: "e27c036b-0583-4363-808d-1fee07cea594"
-  // firstName: {value: "Jane", icon: null}
-  // lastName: {value: "Doe", icon: null}
-  // favorite: true
-  // quickNote: {value: "Likes the cold side of the pillow.", icon: FiClipboard}
-  // birthday: {value: "January 1st, 2222", icon: HiOutlineCake}
-  // location: {value: "Austin, TX USA", icon: FiMapPin}
-  // address: {value: "123 Blvd Street", icon: FiHome}
-  // phoneNumber: {value: "111-222-3333", icon: FiSmartphone}
-  // email: {value: "jane@doe.com", icon: FiMail}
-  // userId: "ckrdw9x400008w2vk5j47mlqm"
-  // groups: []
 
   return sectionData;
 };
